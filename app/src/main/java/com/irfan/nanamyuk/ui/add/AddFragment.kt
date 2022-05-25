b0bc6920da490c6b7544d826e11369bed033de51
Muhammad Irfan <irfan2001@mhs.unsyiah.ac.id>
2022-05-25 13:19:05 +0700

    add bottom navigation

---
+++ app/src/main/java/com/irfan/nanamyuk/ui/add/AddFragment.kt
@@ -0,0 +1,42 @@
+package com.irfan.nanamyuk.ui.add
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.irfan.nanamyuk.databinding.FragmentAddBinding
+
+class AddFragment : Fragment() {
+
+    private var _binding: FragmentAddBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(AddViewModel::class.java)
+
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textAdd
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/irfan/nanamyuk/ui/dash/DashViewModel.kt
@@ -0,0 +1,10 @@
+package com.irfan.nanamyuk.ui.dash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashViewModel : ViewModel() {
+
+
+}
