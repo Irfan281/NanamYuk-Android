d820d5a2f01991a72dff633ab5ae4186edaadfe7
Muhammad Rifki Aufa <rifki_aufa@mhs.unsyiah.ac.id>
2022-05-25 17:55:42 +0700

    Menambah pada Add Fragment

--- app/src/main/java/com/irfan/nanamyuk/ui/add/AddFragment.kt
+++ app/src/main/java/com/irfan/nanamyuk/ui/add/AddFragment.kt
@@ -22,17 +22,8 @@ class AddFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(AddViewModel::class.java)
-
         _binding = FragmentAddBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textAdd
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/irfan/nanamyuk/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.irfan.nanamyuk.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.irfan.nanamyuk.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
