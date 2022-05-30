9ac693a581b4aced04bcdf601ea268d92db1e47d
Muhammad Rifki Aufa <rifki_aufa@mhs.unsyiah.ac.id>
2022-05-31 00:31:36 +0700

    edit AddFragment
    Menambah responsif pada radio button di halaman AddFragment

--- app/src/main/java/com/irfan/nanamyuk/ui/add/AddFragment.kt
+++ app/src/main/java/com/irfan/nanamyuk/ui/add/AddFragment.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.irfan.nanamyuk.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
     private var _binding: FragmentAddBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var addMethod = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +22,28 @@ class AddFragment : Fragment() {
     ): View {
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextButton.setOnClickListener {
+            val addMethodID = binding.rgMethod.checkedRadioButtonId
+
+            if(addMethodID != -1) {
+                addMethod = resources.getResourceEntryName(addMethodID)
+            }
+
+            when(addMethod){
+                "rbRecom" -> {
+                    Toast.makeText(activity, "Ini rekomendasi", Toast.LENGTH_SHORT).show()
+                }"rbChoice" -> {
+                    Toast.makeText(activity, "Ini pilih tanaman", Toast.LENGTH_SHORT).show()
+                }else -> {
+                    Toast.makeText(activity, "Silakan pilih dulu opsi yang tersedia", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
