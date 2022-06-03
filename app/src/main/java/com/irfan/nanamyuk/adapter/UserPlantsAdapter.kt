0df93bb95728e1c1a258558f33f5cf4aa196d76f
Muhammad Rifki Aufa <rifki_aufa@mhs.unsyiah.ac.id>
2022-06-03 18:01:18 +0700

    mengedit water status card
    Memisahkan onclick pada card dan tombol siram

--- app/src/main/java/com/irfan/nanamyuk/adapter/UserPlantsAdapter.kt
+++ app/src/main/java/com/irfan/nanamyuk/adapter/UserPlantsAdapter.kt
@@ -3,14 +3,17 @@ package com.irfan.nanamyuk.adapter
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.irfan.nanamyuk.R
 import com.irfan.nanamyuk.databinding.ItemStatusBinding
 import com.irfan.nanamyuk.ui.detail.DetailActivity
 
 class UserPlantsAdapter(private val datas: List<String>) :
-    RecyclerView.Adapter<UserPlantsAdapter.ViewHolder>() {
+    RecyclerView.Adapter<UserPlantsAdapter.ViewHolder>(), View.OnClickListener {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding =
@@ -23,14 +26,23 @@ class UserPlantsAdapter(private val datas: List<String>) :
 
         holder.binding.textView.text = name
 
-        holder.itemView.setOnClickListener{
-            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
-            holder.itemView.context.startActivity(intent)
-        }
+        holder.binding.card.setOnClickListener(this)
+        holder.binding.fabWater.setOnClickListener(this)
     }
 
     override fun getItemCount(): Int = datas.size
 
     inner class ViewHolder(var binding: ItemStatusBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.fab_water -> {
+                Toast.makeText(view.context, "Ini tombol water", Toast.LENGTH_SHORT).show()
+            }R.id.card -> {
+                val i = Intent(view.context, DetailActivity::class.java)
+                view.context.startActivity(i)
+            }
+        }
+    }
 
 }
